5a97bd1b21a4144a5183fe9361e8d85fbc661176
unknown <benjaminghv@gmail.com>
2026-06-15 22:42:39 -0400

    fix: project example

--- cortexApp/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const { stdin, stdout } = process;
-const rl = readline.createInterface({ input: stdin, output: stdout });
-
-let tasks = [];
-
-rl.question('Enter a task (or /no_think /think"
